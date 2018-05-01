16e5e5143545c6751499e13c12f7edf6ab3142b8
ShaunCayabyab <Shaun.C397@gmail.com>
2018-04-30 20:12:29 -0400

    initial-develop | added modalbox for media

--- public/src/js/modal-handler.js
+++ public/src/js/modal-handler.js
@@ -1,1 +1,18 @@
-//
+$(document).ready(function () {
+    $('.thumbnail').click(function () {
+        const thumbnail_id  = $(this).attr('id');
+        const data_id       = $(this).data('thumbnail-id');
+        let $modal_box      = $('.remodal');
+
+        $modal_box.attr('data-remodal-id', thumbnail_id);
+        $modal_box.html(media_data[data_id]["iframe"]);
+
+        let $remodal = $(`[data-remodal-id=${thumbnail_id}]`).remodal();
+        $remodal.open();
+    });
+
+    
+    $(document).on('closed', '.remodal', function (e) {
+        $(this).html('');
+    });
+});
