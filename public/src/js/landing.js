ac718bac46e617664a6c5a9c7833821fb6361754
ShaunCayabyab <Shaun.C397@gmail.com>
2018-05-03 23:11:18 -0400

    initial-develop | just spicin' up the landing page

--- public/src/js/landing.js
+++ public/src/js/landing.js
@@ -1,8 +1,15 @@
 $(document).ready(function () {
-	$('.works-thumbnails').slick({
-		autoplay     : true,
-		autoplaySpeed: 4000,
-		dots         : true,
-		variableWidth: false,
+	$('body, header, footer').hide();
+
+	$('body').fadeIn(400, () => {
+		$('header').fadeIn(700, () => {
+			$('footer').fadeIn(700);
+			$('.works-thumbnails').slick({
+				autoplay     : true,
+				autoplaySpeed: 4000,
+				dots         : true,
+				variableWidth: false,
+			});
+		});
 	});
 })
